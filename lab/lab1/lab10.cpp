47b10831d94a6f55b01554548ab70aab42f86890
Nicholas Weekes <weekes.nicholas91@gmail.com>
2020-05-11 18:57:01 -0400

    Add files via upload

---
+++ lab/lab1/lab10.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+/**Define Functions Below This Comment**/
+// Do: Use a BOOL function
+
+// Pass two parameters "S1, S2" in the function
+
+//Use "If statement", to iterate throough the number to determine if it is a binary
+
+// else: "if" it is not a binary, just return " false"
+
+// Use "else" to detect if the number both equal he same  charcter length,output "0"
+
+// if the character size at a static set posission and, it will return a long string of 0's
+
+
+/**Define Functions Above This Comment**/
+    // USE BOOL function
+
+// Pass two parameters called "P1, P2"
+
+// set an "if" statement that will check to detect if the values are not equal
+
+// Set "else" to return 0 or false if the are not equal to 0 or 1
+
+int main()
+{
+	return 0;
+}
